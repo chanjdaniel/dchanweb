eae36aee1d239a36951280f450c82b4636bd7a9e
Daniel Chan <chanjdaniel@gmail.com>
2022-08-15 14:17:02 -0700

    initial set up

---
+++ portfolio/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav_container">
+        <header>
+          <ul>
+            <li><a>Contact</a></li>
+            <li><a>Experience</a></li>
+            <li><a>Projects</a></li>
+            <li><a>About</a></li>
+          </ul>
+        </header>
+      </div>
+      <body className="App-header">
+        <p>
+          DANIEL CHAN
+        </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
